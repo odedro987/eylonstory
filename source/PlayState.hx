2a5a4a7b3c62f525c7f245a647dffb57a232d07d
Oded Rosenblatt <odedro987@gmail.com>
2023-04-08 17:48:15 +0200

    add bg, skip splash screen

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,7 @@
 package;
 
+import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
 
 class PlayState extends FlxState
@@ -7,6 +9,13 @@ class PlayState extends FlxState
 	override public function create()
 	{
 		super.create();
+
+		var bg = new FlxSprite(0, 0);
+		bg.makeGraphic(FlxG.width, FlxG.height, 0xff148dd3);
+		add(bg);
+		var floor = new FlxSprite(0, 380);
+		floor.makeGraphic(FlxG.width, 100, 0xff098019);
+		add(floor);
 	}
 
 	override public function update(elapsed:Float)
